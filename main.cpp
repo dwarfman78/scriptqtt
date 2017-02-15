6b94c0da2dfa53376e4d4505b6fe8f97832df43b
fabien <toto>
2017-02-15 22:26:44 +0100

    add script capabilities

--- main.cpp
+++ main.cpp
@@ -2,17 +2,40 @@
 
 #include "mqtt/async_client.h"
 
+#include <chaiscript/chaiscript.hpp>
+
+#include <chaiscript/chaiscript_stdlib.hpp>
+
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
 
+    chaiscript::ChaiScript mChai;
+
     mqtt::async_client client({"tcp://192.168.1.248:1883"}, {"CLIENT"});
 
-    mqtt::itoken_ptr conntok = client.connect();
+    mChai.add(chaiscript::var(&client),"client");
+
+    //chaiscript::fun<ReturnType (ParamType1, ParamType2)>(&function_with_overloads)
+    //chaiscript::fun([this](const std::string& who, const std::string& something){ say(who,something); })
+
+    //mChai.add(chaiscript::fun(&mqtt::async_client::connect,std::ref(client)),"connect");
+
+    //mChai.add(chaiscript::base_class<mqtt::iasync_client, mqtt::async_client>());
+
+    mChai.add(chaiscript::fun<mqtt::itoken_ptr,mqtt::async_client>(&mqtt::async_client::connect),"connect");
+
+    mChai.add(chaiscript::fun<void,mqtt::itoken>(&mqtt::itoken::wait_for_completion),"wait_for_completion");
+    //mChai.add(chaiscript::fun<mqtt::itoken_ptr (mqtt::async_client::*) ()>(&mqtt::async_client::connect),"connect");
+
+    mChai.eval_file("scripts/connectMe.chai");
+
+
+    /*mqtt::itoken_ptr conntok = client.connect();
     std::cout << "Waiting for the connection..." << std::flush;
     conntok->wait_for_completion();
-    std::cout << "OK" << std::endl;
+    std::cout << "OK" << std::endl;*/
 
 
     std::cout << "Sending message..." << std::flush;
