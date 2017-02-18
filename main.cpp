8ccb3493ab190a509474fa5084fe6b96aa349c64
fabien <toto>
2017-02-18 23:34:39 +0100

    mosquitto

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "mqtt/async_client.h"
+#include <mosquittopp.h>
 
 #include <chaiscript/chaiscript.hpp>
 
@@ -11,11 +11,11 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
 
-    chaiscript::ChaiScript mChai;
+    chaiscript::ChaiScript mChai(chaiscript::Std_Lib::library());
 
-    mqtt::async_client client({"tcp://192.168.1.248:1883"}, {"CLIENT"});
 
-    mChai.add(chaiscript::var(&client),"client");
+
+    //mChai.add(chaiscript::var(&client),"client");
 
     //chaiscript::fun<ReturnType (ParamType1, ParamType2)>(&function_with_overloads)
     //chaiscript::fun([this](const std::string& who, const std::string& something){ say(who,something); })
@@ -24,25 +24,20 @@ int main() {
 
     //mChai.add(chaiscript::base_class<mqtt::iasync_client, mqtt::async_client>());
 
-    mChai.add(chaiscript::fun<mqtt::itoken_ptr,mqtt::async_client>(&mqtt::async_client::connect),"connect");
+    //mChai.add(chaiscript::fun<mqtt::itoken_ptr,mqtt::async_client>(&mqtt::async_client::connect),"connect");
 
-    mChai.add(chaiscript::fun<void,mqtt::itoken>(&mqtt::itoken::wait_for_completion),"wait_for_completion");
+    //mChai.add(chaiscript::fun<void,mqtt::itoken>(&mqtt::itoken::wait_for_completion),"wait_for_completion");
     //mChai.add(chaiscript::fun<mqtt::itoken_ptr (mqtt::async_client::*) ()>(&mqtt::async_client::connect),"connect");
 
-    mChai.eval_file("scripts/connectMe.chai");
+    //mChai.eval_file("scripts/connectMe.chai");
 
+    mosqpp::mosquittopp mosquitto("CLIENT",true);
 
-    /*mqtt::itoken_ptr conntok = client.connect();
-    std::cout << "Waiting for the connection..." << std::flush;
-    conntok->wait_for_completion();
-    std::cout << "OK" << std::endl;*/
+    mosquitto.connect("192.168.1.248");
 
+    mosquitto.publish(nullptr,"home/cuve",1,"1",1,false);
 
-    std::cout << "Sending message..." << std::flush;
-    mqtt::message_ptr pubmsg = std::make_shared<mqtt::message>("coucou");
-    pubmsg->set_qos(1);
-    client.publish("home/cuve", pubmsg)->wait_for_completion(10);
-    std::cout << "OK" << std::endl;
+    mosquitto.disconnect();
 
     return 0;
 }
